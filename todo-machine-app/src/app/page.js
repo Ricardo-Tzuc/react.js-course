3065ef9510c58342ff7bc4dd798c9244c1ad3610
Ricardo-Tzuc <ricardotz9911@gmail.com>
2024-05-31 21:27:39 -0600

    Component understanding: empty components and components with props are added.

--- todo-machine-app/src/app/page.js
+++ todo-machine-app/src/app/page.js
@@ -1,10 +1,46 @@
-
 import styles from "./page.module.css";
 
+//  Components are Capitalized
 export default function Home() {
   return (
     <main className={styles.main}>
+      {/* <h1> Hello World!</h1> -React Elements */}
       <h1> Hello World!</h1>
+     <TodoItem />
+     <TodoItem />
+     <TodoItem />
+
+
+      {/*This is an example component which receives "props"
+      props: properties written as objects which helps
+      to be more dynamic our applications  */}
+     <TodoCount
+     completed={8}
+     total={16}
+     />
+     <TodoCount
+     completed={10}
+     total={14}
+     />
     </main>
   );
+}
+
+// New Component
+function  TodoItem(){
+  return (
+    <li>
+      <span>V</span>
+      <p>Cry while codding</p>
+      <span>X</span>
+    </li>
+  );
+}
+// Component with Props
+function TodoCount(props) {
+  return(
+    <h1>
+      You completed {props.completed} of {props.total} TODOs
+    </h1>
+  )
 }

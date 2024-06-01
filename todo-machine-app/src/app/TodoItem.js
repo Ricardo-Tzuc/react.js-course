cd22e01ab433108ffe41f82cffb15bcb434acb4e
Ricardo-Tzuc <ricardotz9911@gmail.com>
2024-06-01 00:12:06 -0600

    Components separated by JS files

---
+++ todo-machine-app/src/app/TodoItem.js
@@ -0,0 +1,11 @@
+// Component list items
+function  TodoItem(){
+    return (
+      <li>
+        <span>V</span>
+        <p>Cry while codding</p>
+        <span>X</span>
+      </li>
+    );
+  }
+export { TodoItem }  
--- todo-machine-app/src/app/page.js
+++ todo-machine-app/src/app/page.js
@@ -1,46 +1,27 @@
+import { TodoCounter } from './TodoCounter'
+import { TodoSearch } from './TodoSearch'
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 import styles from "./page.module.css";
 
 //  Components are Capitalized
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <h1> Hello World!</h1> -React Elements */}
-      <h1> Hello World!</h1>
-     <TodoItem />
-     <TodoItem />
-     <TodoItem />
+      {/* Technique to Create fist the components structure,
+      later create the components functions.*/}
+      <TodoCounter />
+      <TodoSearch />
 
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
 
-      {/*This is an example component which receives "props"
-      props: properties written as objects which helps
-      to be more dynamic our applications  */}
-     <TodoCount
-     completed={8}
-     total={16}
-     />
-     <TodoCount
-     completed={10}
-     total={14}
-     />
+      <CreateTodoButton />
+
     </main>
   );
-}
-
-// New Component
-function  TodoItem(){
-  return (
-    <li>
-      <span>V</span>
-      <p>Cry while codding</p>
-      <span>X</span>
-    </li>
-  );
-}
-// Component with Props
-function TodoCount(props) {
-  return(
-    <h1>
-      You completed {props.completed} of {props.total} TODOs
-    </h1>
-  )
 }

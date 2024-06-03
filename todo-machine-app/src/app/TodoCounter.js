a7c9d640945d1a1fec199837519e1700f9806798
Ricardo-Tzuc <ricardotz9911@gmail.com>
2024-06-03 15:52:03 -0600

    inline styles added: passed as an object inside the component

--- todo-machine-app/src/app/TodoCounter.js
+++ todo-machine-app/src/app/TodoCounter.js
@@ -1,11 +1,14 @@
-const styles = {
-  backgroundColor: 'red'
-}
+
 
 
 function  TodoCounter({total, completed}){
     return (
-      <h1 style={styles}>
+      <h1 style={{
+        fontSize: '24px',
+        textAlign: 'center',
+        margin: 0,
+        padding: '48px',
+      }}>
         You have completed {completed} of {total} TODOS
       </h1>
     );

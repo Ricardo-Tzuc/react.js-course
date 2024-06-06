320137d822faece03285c67461fe557b0c5ff4e4
Ricardo-Tzuc <ricardotz9911@gmail.com>
2024-06-06 00:07:10 -0600

    state manage in todoSearch is added

--- todo-machine-app/src/app/TodoSearch.js
+++ todo-machine-app/src/app/TodoSearch.js
@@ -1,17 +1,30 @@
 'use client';
 import './TodoSearch.css'
+import {useState} from 'react';
 
 function  TodoSearch(){
+
+    // useState is a function, state can be updated and consumed.
+    // useState receive a default initial state to give to the first render
+    const [searchValue, setSearchValue] = useState('');
+    // setSearchValue is an special function to update the state.
+
+    console.log("user wrote: "+ searchValue);
+
     return (
-        <input type="text" placeholder="Cortar cebolla" 
+        <input 
+        type="text"
+        placeholder="Cortar cebolla" 
         className='TodoSearch'
+        
+        // value taken as the first render
+        value={searchValue}
+
         // events MUST be handled by functions
         onChange={(event)=>{
-  
-            console.log('you wrote in the input:')
-            console.log(event.target.value);
+            setSearchValue(event.target.value);
         }}
         />
     );
   }
-  export { TodoSearch }
+  export { TodoSearch };

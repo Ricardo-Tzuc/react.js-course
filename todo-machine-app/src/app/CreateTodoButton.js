a460f320a7258a5bf16e397aa4b78994212cad44
Ricardo-Tzuc <ricardotz9911@gmail.com>
2024-06-05 22:13:39 -0600

    onClick & onChange events added to the interactive components

--- todo-machine-app/src/app/CreateTodoButton.js
+++ todo-machine-app/src/app/CreateTodoButton.js
@@ -1,8 +1,19 @@
+/* Next.js needs this declaration in order to know
+that this is a Client side render component. */
+'use client';
+
 import './CreateTodoButton.css'
+
+// function to handle the click event.
+function handleClick(){
+    console.log('you clicked here');
+}
+
 
 function  CreateTodoButton(){
     return (
-        <button className='CreateTodoButton'>+</button>
+        // arrow functions can be used also in the onClick body.
+        <button className='CreateTodoButton' onClick={handleClick}>+</button>
     );
   }
 
--- todo-machine-app/src/app/TodoSearch.js
+++ todo-machine-app/src/app/TodoSearch.js
@@ -1,10 +1,17 @@
+'use client';
 import './TodoSearch.css'
 
 function  TodoSearch(){
     return (
         <input type="text" placeholder="Cortar cebolla" 
-        className='TodoSearch'/>
+        className='TodoSearch'
+        // events MUST be handled by functions
+        onChange={(event)=>{
+  
+            console.log('you wrote in the input:')
+            console.log(event.target.value);
+        }}
+        />
     );
   }
-
   export { TodoSearch }
